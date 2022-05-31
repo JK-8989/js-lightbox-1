1bb043bf2268186dfd8e238ca60e9de8ae70a913
JK-8989 <jiyoungkim365@gmail.com>
2022-05-31 11:54:18 -0700

    Add images/EventListener/css styling

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener('click', lightbox);
+
+function lightbox(e){
+  let element = e.target,
+      elementId = element.getAttribute('id'),
+      lightBoxImg = document.getElementById('lightbox-image'),
+      lightBoxBg = document.getElementById('lightbox-overlay'),
+      fullImage = new Image();
+
+      if(element.hasAttribute('data-lightbox')){
+          e.preventDefault();
+
+          fullImage.onload = function() {
+            lightBoxImg.src = this.src;
+            // console.log(this.src)
+          }
+          lightBoxImg.src = '';
+          fullImage.src = element.getAttribute('data-lightbox');
+          lightBoxBg.classList.add('visible');
+          // console.log(element.getAttribute('data-lightbox'))
+
+          lightBoxBg.addEventListener('click', ()=>{
+          lightBoxBg.classList.remove('visible')
+          })
+
+        };
+
+        
+      
+      }
+
+
+     
+    
+     
+  // console.log(element)
+ 
+
